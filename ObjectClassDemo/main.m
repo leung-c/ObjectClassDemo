82bde4f301a0ae13cb3f1755c11847e7132cd7b5
liangchao <leung-c@outlook.com>
2017-10-09 14:53:19 +0800

    Initial Commit

---
+++ ObjectClassDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectClassDemo
+//
+//  Created by liangchao on 17/10/9.
+//  Copyright © 2017年 lch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
